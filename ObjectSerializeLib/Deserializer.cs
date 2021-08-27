b9bb37fe2e835649df403156bef16ac1020ca3b2
arilhr <aurielharlih9213@gmail.com>
2021-08-27 18:22:25 +0700

    Serialize and Deserialize Data

---
+++ ObjectSerializeLib/Deserializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ObjectSerializeLib
+{
+    public class Deserializer
+    {
+        
+        public PlayerData DeserializeData(string path)
+        {
+            // make a blank player data
+            PlayerData data = new PlayerData();
+
+            if (File.Exists(path))
+            {
+                // initialize binary formatter
+                BinaryFormatter formatter = new BinaryFormatter();
+                // open file in path
+                FileStream stream = new FileStream(path, FileMode.Open);
+
+                // deserialize data
+                data = formatter.Deserialize(stream) as PlayerData;
+
+                // close file
+                stream.Close();
+            }
+            else
+            {
+                Console.WriteLine("file not found..");
+            }
+
+            return data;
+        }
+    }
+}
---
+++ ObjectSerializeLib/PlayerData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectSerializeLib
+{
+    [Serializable]
+    public class PlayerData
+    {
+        public string playerName;
+        public int level;
+        public int gold;
+
+        public PlayerData() { }
+
+        public PlayerData(string playerName, int level, int gold)
+        {
+            this.playerName = playerName;
+            this.level = level;
+            this.gold = gold;
+        }
+
+        public void InputData()
+        {
+            Console.Write("Enter player name: ");
+            playerName = Console.ReadLine();
+            Console.Write("Enter player level: ");
+            level = int.Parse(Console.ReadLine());
+            Console.Write("Enter player gold: ");
+            gold = int.Parse(Console.ReadLine());
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine(playerName);
+            Console.WriteLine(level);
+            Console.WriteLine(gold);
+        }
+    }
+}
